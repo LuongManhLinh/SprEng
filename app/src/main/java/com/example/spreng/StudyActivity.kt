8946385da7b5354ce8df123d7ee292c6b2d57020
22021195BangVanChien <chienlgend24032004@gmail.com>
2024-11-20 07:53:28 +0700

    add listeningqs

--- app/src/main/java/com/example/spreng/StudyActivity.kt
+++ app/src/main/java/com/example/spreng/StudyActivity.kt
@@ -5,10 +5,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import com.example.spreng.ui.studyscreen.BaseStudyScreen
 import com.example.spreng.ui.studyscreen.answer.wordpicker.WordPickerFillScreen
 import com.example.spreng.ui.studyscreen.answer.wordpicker.WordPickerSequenceScreen
+import com.example.spreng.ui.studyscreen.answer.writing.BaseWritingScreen
+import com.example.spreng.ui.studyscreen.question.listening.BaseListeningQuestion
 import com.example.spreng.ui.theme.SprEngTheme
+import java.lang.reflect.Modifier
 
 class StudyActivity : ComponentActivity() {
 
@@ -17,7 +22,17 @@ class StudyActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SprEngTheme {
-
+                BaseStudyScreen(
+//                    modifier = Modifier.fillMaxSize(),
+                    learningProgress = 1.2F,
+                    questionTitle = "HelloStudy",
+                    onCancelling = {},
+                    onCompleting = {},
+                    content = {
+                        BaseListeningQuestion()
+                        BaseWritingScreen()
+                    }
+                )
             }
         }
     }
